501a868cdb27c34a97b65007fc9da561cb349f1f
Justas Lavišius <bucaneer@gmail.com>
2016-11-20 17:39:04 +0200

    restructure: rename fleditor.py -> editorwindow.py

--- flint/__main__.py
+++ flint/__main__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flint.fleditor import FlGlob, EditorWindow, log
+from flint.editorwindow import FlGlob, EditorWindow, log
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
