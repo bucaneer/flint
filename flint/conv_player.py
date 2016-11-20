5c46a001b328549997686e182882a158ed8a7ea3
Justas Lavišius <bucaneer@gmail.com>
2016-11-20 13:10:49 +0200

    update copyright years

--- flint/conv_player.py
+++ flint/conv_player.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2015 Justas Lavišius
+# Copyright (C) 2015, 2016 Justas Lavišius
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
